d24c847c18fb78f781c6097bea6aa09c4c28fe41
Fahrudin Sljivo <fsljivo@nagax.com>
2024-11-23 13:54:59 +0100

    Initial Commit

---
+++ SynonymsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SynonymsApp
+//
+//  Created by Fahrudin Sljivo on 23. 11. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SynonymsApp/SynonymsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynonymsAppApp.swift
+//  SynonymsApp
+//
+//  Created by Fahrudin Sljivo on 23. 11. 2024..
+//
+
+import SwiftUI
+
+@main
+struct SynonymsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
